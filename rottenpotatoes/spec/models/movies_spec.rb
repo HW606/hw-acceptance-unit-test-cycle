ad1bcef72817da7bcac7ce979a33f5e0c32de700
EC2 Default User <ec2-user@ip-172-31-35-250.us-east-2.compute.internal>
2021-03-12 01:28:26 +0000

    final version

--- rottenpotatoes/spec/models/movies_spec.rb
+++ rottenpotatoes/spec/models/movies_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Movies, type: :model do
-  describe 'searching similar directors' do
-    it 'should call Movie with director' do
-      Movie.should_receive(:similar_directors).with('Star Wars')
-      Movie.similar_directors('Star Wars')
-    end
+
+describe 'searching similar directors' do
+  it 'should call Movie with director' do
+    Movie.should_receive(:similar_directors).with('Star Wars')
+    Movie.similar_directors('Star Wars')
   end
+  
 end
